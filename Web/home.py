dde2a7cdffa9f3ec307c8188e27e4242e1edfd0b
maanvi24 <113322629+maanvi24@users.noreply.github.com>
2023-12-14 17:51:42 -0800

    Update home.py

--- Web/home.py
+++ Web/home.py
@@ -12,6 +12,8 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Stock Prediction App</h1>", unsafe_allow_html=True)
 # Page title
+
+
 st.markdown("""
 We strive to make stock market insights easy and accessible for everyone. Explore our platform to make informed decisions in the dynamic world of stocks!
 """)
@@ -25,11 +27,11 @@ st.markdown("Input an article and press predict to know if the stock will go up 
 st.markdown("You will also get other relevant information such as the likelihood of the stock going up or down, sentiment score to understand the tone of the article and a score that tells the relevance of the article to a certain field like technology,etc")
 
 # Live Feed section
-st.subheader("Live Feed Page:")
+st.markdown("- **Live Feed Page:**")
 st.markdown("Input a stock ticker of your choice for example AAPL( Apple Stock) and duration to get all the relevant articles and some more useful information.")
 
 # Summary Page section
-st.subheader("Summary Page:")
+st.markdown("- **Summary Page:**")
 st.markdown("Input an article and the desired length to get a quick overview of complex information.")
 
 
